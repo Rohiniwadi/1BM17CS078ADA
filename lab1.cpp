3e523418dbf6846e8e45735f2c2871d45546db4d
Rohiniwadi <53818416+Rohiniwadi@users.noreply.github.com>
2019-08-07 12:33:20 +0530

    Add files via upload

---
+++ lab1.cpp
@@ -0,0 +1,42 @@
+#include<iostream>
+using namespace std;
+
+
+int  main()
+{
+  int t,k,N,a[100];
+  cin>>t;
+  for(int i=0;i<t;i++)
+  {
+    cin>>N>>k;
+    for(int j=0;j<N;j++)
+     {
+       cin>>a[j];
+     }
+     find(N,k,a);
+   } 
+ return 0;
+}
+ 
+void find(int N,int k,int a[])
+{
+  int f=0;
+  int l=N-1;
+  int m;
+  m=(l+f)/2;
+  while(f<=l)
+   {
+     if(k == a[m])
+      { cout<<"1";}
+     else if(k<a[m])
+      { l=m-1;}
+     else
+      {f=m+1;}
+   }
+   if(f>l)
+   { cout<<"-1";}
+}
+    
+
+     
+
