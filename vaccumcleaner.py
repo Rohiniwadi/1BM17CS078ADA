d7bbe45f54a26f44d5329f72b9636e10512e0e72
Rohiniwadi <53818416+Rohiniwadi@users.noreply.github.com>
2020-05-07 19:23:29 +0530

    Update vaccumcleaner.py

--- vaccumcleaner.py
+++ vaccumcleaner.py
@@ -5,34 +5,34 @@ d['A'] = int(input())
 d['B'] = int(input())
 print(d)
 vloc = int(input())
-while (d['A']!=0 and d['B']!=0):
+while (d['A']!=0 or d['B']!=0):
     if vloc==0:
         if d['A']==1:
-            print("location A id dirty")
+            print("location A is dirty")
             d['A']=0
             print('location A cleaned')
             print('moving to location B')
             if d['B']==1:
-                d['B']==0
+                d['B']=0
                 print('location B cleaned')
         else:
             print('moving to location B')
             if d['B']==1:
-                d['B']==0
+                d['B']=0
                 print('location B cleaned')
+
     else:
         if d['B']==1:
-            print("location B id dirty")
-            d['A']=0
+            print("location B is dirty")
+            d['B']=0
             print('location B cleaned')
             print('moving to location A')
             if d['A']==1:
-                d['A']==0
+                d['A']=0
                 print('location A cleaned')
         else:
             print('moving to location A')
             if d['A']==1:
-                d['A']==0
+                d['A']=0
                 print('location A cleaned')
-
-
+    
