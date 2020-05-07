2f5edc6a832e9d21e27d5d86185c3ed675978e69
Rohiniwadi <53818416+Rohiniwadi@users.noreply.github.com>
2020-05-07 19:00:33 +0530

    Update tic-tac-toe.py

--- tic-tac-toe.py
+++ tic-tac-toe.py
@@ -1,83 +1,109 @@
 from tkinter import *
 import tkinter.messagebox
+tk = Tk()
+tk.title("Tic Tac Toe")
 
-root = Tk()
-root.title("TIC_TAC_TOE")
 
-flag=0
-bclick=True
+
+
+
+
+bclick = True
+flag = 0
+
 def disableButton():
-    b1.configure(state=DISABLED)
-    b2.configure(state=DISABLED)
-    b3.configure(state=DISABLED)
-    b4.configure(state=DISABLED)
-    b5.configure(state=DISABLED)
-    b6.configure(state=DISABLED)
-    b7.configure(state=DISABLED)
-    b8.configure(state=DISABLED)
-    b9.configure(state=DISABLED)
-    
-def btnclk(buttons):
-    global flag,bclick
+    button1.configure(state=DISABLED)
+    button2.configure(state=DISABLED)
+    button3.configure(state=DISABLED)
+    button4.configure(state=DISABLED)
+    button5.configure(state=DISABLED)
+    button6.configure(state=DISABLED)
+    button7.configure(state=DISABLED)
+    button8.configure(state=DISABLED)
+    button9.configure(state=DISABLED)
+
+
+
+def btnClick(buttons):
+    global bclick, flag
     if buttons["text"] == " " and bclick == True:
         buttons["text"] = "X"
-        bclick=False
-        check()
-        flag+=1
-    elif buttons['text']==" " and bclick==False:
-        buttons['text']='O'
-        bclick=True
-        check()
-        flag+=1
+        bclick = False
+        checkForWin()
+        flag += 1
+
+
+    elif buttons["text"] == " " and bclick == False:
+        buttons["text"] = "O"
+        bclick = True
+        checkForWin()
+        flag += 1
     else:
         tkinter.messagebox.showinfo("Tic-Tac-Toe", "Button already Clicked!")
 
-def check():
-    if (b1['text']=='X'and b2['text']=='X' and b3['text']=='X'or
-        b1['text']=='X'and b5['text']=='X' and b9['text']=='X' or
-        b1['text']=='X'and b4['text']=='X' and b7['text']=='X' or
-        b2['text']=='X'and b5['text']=='X' and b8['text']=='X' or
-        b3['text']=='X'and b6['text']=='X' and b9['text']=='X' or
-        b3['text']=='X'and b5['text']=='X' and b7['text']=='X' or
-        b4['text']=='X'and b5['text']=='X' and b6['text']=='X' or
-        b7['text']=='X'and b8['text']=='X' and b9['text']=='X' ):
+def checkForWin():
+    if (button1['text'] == 'X' and button2['text'] == 'X' and button3['text'] == 'X' or
+        button4['text'] == 'X' and button5['text'] == 'X' and button6['text'] == 'X' or
+        button7['text'] =='X' and button8['text'] == 'X' and button9['text'] == 'X' or
+        button1['text'] == 'X' and button5['text'] == 'X' and button9['text'] == 'X' or
+        button3['text'] == 'X' and button5['text'] == 'X' and button7['text'] == 'X' or
+        button1['text'] == 'X' and button2['text'] == 'X' and button3['text'] == 'X' or
+        button1['text'] == 'X' and button4['text'] == 'X' and button7['text'] == 'X' or
+        button2['text'] == 'X' and button5['text'] == 'X' and button8['text'] == 'X' or
+        button7['text'] == 'X' and button6['text'] == 'X' and button9['text'] == 'X'):
         disableButton()
-        tkinter.messagebox.showinfo("Tic-Tac-Toe", 'Player 1 Won!')
-    elif([b1['text']=='X'and b2['text']=='X' and b3['text']=='X'] or
-        [b1['text']=='X'and b5['text']=='X' and b9['text']=='X'] or
-        [b1['text']=='X'and b4['text']=='X' and b7['text']=='X'] or
-        [b2['text']=='X'and b5['text']=='X' and b8['text']=='X'] or
-        [b3['text']=='X'and b6['text']=='X' and b9['text']=='X'] or
-        [b3['text']=='X'and b5['text']=='X' and b7['text']=='X'] or
-        [b4['text']=='X'and b5['text']=='X' and b6['text']=='X'] or
-        [b7['text']=='X'and b8['text']=='X' and b9['text']=='X'] ):
+        tkinter.messagebox.showinfo("Tic-Tac-Toe", 'player1')
+
+    elif(flag == 8):
+        tkinter.messagebox.showinfo("Tic-Tac-Toe", "It is a Tie")
+
+    elif (button1['text'] == 'O' and button2['text'] == 'O' and button3['text'] == 'O' or
+          button4['text'] == 'O' and button5['text'] == 'O' and button6['text'] == 'O' or
+          button7['text'] == 'O' and button8['text'] == 'O' and button9['text'] == 'O' or
+          button1['text'] == 'O' and button5['text'] == 'O' and button9['text'] == 'O' or
+          button3['text'] == 'O' and button5['text'] == 'O' and button7['text'] == 'O' or
+          button1['text'] == 'O' and button2['text'] == 'O' and button3['text'] == 'O' or
+          button1['text'] == 'O' and button4['text'] == 'O' and button7['text'] == 'O' or
+          button2['text'] == 'O' and button5['text'] == 'O' and button8['text'] == 'O' or
+          button7['text'] == 'O' and button6['text'] == 'O' and button9['text'] == 'O'):
         disableButton()
-        tkinter.messagebox.showinfo("Tic-Tac-Toe", 'Player 1 Won!')
-    elif (flag==8):
-        tkinter.messagebox.showinfo("Tic-Tac-Toe", "it's a TIE")
-        
+        tkinter.messagebox.showinfo("Tic-Tac-Toe", 'player2')
+
 
 buttons = StringVar()
-l1=Label(root,text='Player 1: X').grid(row=0,column=1)
-l2=Label(root,text='Player 2: O').grid(row=0,column=3)
 
-b1=Button(root,text=' ',font='Times 20 bold',bg='GREY',fg='WHITE',height=3,width=5,command=lambda: btnclk(b1))
-b1.grid(row=1,column=1)
-b2=Button(root,text=' ',font='Times 20 bold',bg='GREY',fg='WHITE',height=3,width=5,command=lambda:btnclk(b2))
-b2.grid(row=1,column=2)
-b3=Button(root,text=' ',font='Times 20 bold',bg='GREY',fg='WHITE',height=3,width=5,command=lambda:btnclk(b3))
-b3.grid(row=1,column=3)
-b4=Button(root,text=' ',font='Times 20 bold',bg='GREY',fg='WHITE',height=3,width=5,command=lambda:btnclk(b4))
-b4.grid(row=2,column=1)
-b5=Button(root,text=' ',font='Times 20 bold',bg='GREY',fg='WHITE',height=3,width=5,command=lambda:btnclk(b5))
-b5.grid(row=2,column=2)
-b6=Button(root,text=' ',font='Times 20 bold',bg='GREY',fg='WHITE',height=3,width=5,command=lambda:btnclk(b6))
-b6.grid(row=2,column=3)
-b7=Button(root,text=' ',font='Times 20 bold',bg='GREY',fg='WHITE',height=3,width=5,command=lambda:btnclk(b7))
-b7.grid(row=3,column=1)
-b8=Button(root,text=' ',font='Times 20 bold',bg='GREY',fg='WHITE',height=3,width=5,command=lambda:btnclk(b8))
-b8.grid(row=3,column=2)
-b9=Button(root,text=' ',font='Times 20 bold',bg='GREY',fg='WHITE',height=3,width=5,command=lambda:btnclk(b9))
-b9.grid(row=3,column=3)
+label = Label( tk, text="Player 1:", font='Times 20 bold', bg='white', fg='black', height=1, width=8)
+label.grid(row=1, column=0)
 
-root.mainloop()
+
+label = Label( tk, text="Player 2:", font='Times 20 bold', bg='white', fg='black', height=1, width=8)
+label.grid(row=2, column=0)
+
+button1 = Button(tk, text=" ", font='Times 20 bold', bg='gray', fg='white', height=4, width=8, command=lambda: btnClick(button1))
+button1.grid(row=3, column=0)
+
+button2 = Button(tk, text=' ', font='Times 20 bold', bg='gray', fg='white', height=4, width=8, command=lambda: btnClick(button2))
+button2.grid(row=3, column=1)
+
+button3 = Button(tk, text=' ',font='Times 20 bold', bg='gray', fg='white', height=4, width=8, command=lambda: btnClick(button3))
+button3.grid(row=3, column=2)
+
+button4 = Button(tk, text=' ', font='Times 20 bold', bg='gray', fg='white', height=4, width=8, command=lambda: btnClick(button4))
+button4.grid(row=4, column=0)
+
+button5 = Button(tk, text=' ', font='Times 20 bold', bg='gray', fg='white', height=4, width=8, command=lambda: btnClick(button5))
+button5.grid(row=4, column=1)
+
+button6 = Button(tk, text=' ', font='Times 20 bold', bg='gray', fg='white', height=4, width=8, command=lambda: btnClick(button6))
+button6.grid(row=4, column=2)
+
+button7 = Button(tk, text=' ', font='Times 20 bold', bg='gray', fg='white', height=4, width=8, command=lambda: btnClick(button7))
+button7.grid(row=5, column=0)
+
+button8 = Button(tk, text=' ', font='Times 20 bold', bg='gray', fg='white', height=4, width=8, command=lambda: btnClick(button8))
+button8.grid(row=5, column=1)
+
+button9 = Button(tk, text=' ', font='Times 20 bold', bg='gray', fg='white', height=4, width=8, command=lambda: btnClick(button9))
+button9.grid(row=5, column=2)
+
+tk.mainloop()
